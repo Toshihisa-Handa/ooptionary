02c6cc3a8750ba9a10a663c9c2c55e99db8083cf
Toshihisa-Handa <waykiyoharu@yahoo.co.jp>
2020-07-04 17:09:31 +0900

    3回目
    マイページに共通ヘッダーを追加

---
+++ team/myOops.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+<?php include('header.php') ?>
+<body>
+  <h1>ここに投稿記事を投稿する記述をする</h1>
+</body>
+</html>
