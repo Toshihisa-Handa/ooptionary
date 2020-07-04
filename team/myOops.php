6b227293249b2ff86bc266bbbc6fba3f93174fb4
Toshihisa-Handa <waykiyoharu@yahoo.co.jp>
2020-07-04 23:12:17 +0900

    4回目
    マイページに記事作成ページを追加

--- team/myOops.php
+++ team/myOops.php
@@ -1,3 +1,10 @@
+<?php 
+session_start();
+include('funcs.php');//別の階層にfuncs.phpがある場合は「betukaisou/funcs.php」などパスを変えてincludesする
+// loginCheck();
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +13,20 @@
   <title>Document</title>
 <?php include('header.php') ?>
 <body>
-  <h1>ここに投稿記事を投稿する記述をする</h1>
+ 
+<form method="post" action="myOopsInsert.php">
+  <div>
+   <fieldset>
+    <legend>YourOoops!</legend>
+     <label>名前：<input type="text" name="name"></label><br>
+     <label>タイトル<input type="text" name="title"></label><br>
+     <p>内容</p>
+     <label><textArea name="naiyou" rows="4" cols="40"></textArea></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+
+
 </body>
 </html>
--- team/mypage.php
+++ team/mypage.php
@@ -1,3 +1,52 @@
+<?php
+session_start();
+include('funcs.php');//別の階層にfuncs.phpがある場合は「betukaisou/funcs.php」などパスを変えてincludesする
+// loginCheck();
+
+
+//1. DB接続します
+$pdo = db_conn();
+
+
+
+//2．データ登録SQL作成
+//prepare("")の中にはmysqlのSQLで入力したINSERT文を入れて修正すれば良いイメージ
+$stmt = $pdo->prepare("SELECT* FROM user_oops_table ");
+$status = $stmt->execute();
+
+
+//3．データ登録処理後（基本コピペ使用でOK)
+$view='';
+if($status==false){
+  //SQL実行時にエラーがある場合（エラーオブジェクト取得して表示）
+  $error = $stmt->errorInfo();
+  exit("SQLError:".$error[2]);//エラーが起きたらエラーの2番目の配列から取ります。ここは考えず、これを使えばOK
+                             // SQLEErrorの部分はエラー時出てくる文なのでなんでもOK
+}else{
+ //selectデータの数だけ自動でループしてくれる
+ while( $r = $stmt->fetch(PDO::FETCH_ASSOC)){
+  //更新用リンクを埋め込んだ表示コード(元のselect.phpから修正する箇所)
+  $view .='<p>';
+  // $view .='<a href="u_view.php? id='.$r["id"].'">';
+  $view .=$r["indate"].":".$r["name"].":".$r["title"].'<br>'.$r["naiyou"];
+  // $view .='</a>';
+//以下はupdateのリンクタグの記述
+  $view .='  ';
+  $view .='<a href="u_view.php? id='.$r["id"].'">';
+  $view .='[更新]';
+  $view .='</a>';
+//以下はdeleteのリンクタグの記述
+  $view .='  ';
+  $view .='<a href="delete.php? id='.$r["id"].'">';
+  $view .='[削除]';
+  $view .='</a>';
+  $view .='</p>';
+ }
+
+}
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -134,6 +183,13 @@
                          <div class="log-img">
                       </div>
                     </div>
+                    <h3>投稿一覧</h3>
+                        <a href="list.php">登録へ戻る</a>
+                        <p><a href="logout.php">ログアウト</a></p>
+
+                        <p><?=$view?></p>
+
+
                   <!-- 記事の投稿ここまで -->
           </div>
       </div>
